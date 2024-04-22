9ffce7f8ef9eeb0c351bfa556714f0cf413483b8
Yalina Gómez <yalinita1985@gmail.com>
2024-04-22 13:21:47 +0200

    ex3

--- index.js
+++ index.js
@@ -14,4 +14,19 @@ let randomNumber = () => Math.floor(Math.random()* 101) //por 101 para incluir e
 
 console.log (randomNumber())      
 
+// ex3 Ús de 'this' en les funcions de fletxa: Crea una classe person que tingui una propietat name i una funció greet que utilitzi una funció de fletxa. La funció ha d'imprimir una salutació que inclogui el nom de la persona.console.log(Hola, ${this.name})
 
+// let greet = new Person ("Carlos")
+// console.log(Carlos.greet()) // llamando al método
+
+let Carlos = new Person ("Carlos")
+let saludo = (Carlos) => `Buen día, ${Carlos.name}`
+console.log(saludo(Carlos))
+
+let Luci = new Person("Luci")
+let saludo2 = (Luci) => `Hi, ${Luci.name}`
+console.log(saludo2(Luci))
+
+let nuevo = new Person("Pablo")
+let saludo3 = (nuevo) => `Hola, ${nuevo.name}`
+console.log(saludo3(nuevo))
---
+++ person.js
@@ -0,0 +1,9 @@
+class Person {
+    constructor(name) {
+    this.name = name
+    }
+
+    // greet() {
+    //     return `Buen día ${this.name}.`
+    // }  
+}

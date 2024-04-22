2ded7f6eb19dc5e2d03c23e9876b10e04f55f8ba
Yalina Gómez <yalinita1985@gmail.com>
2024-04-22 12:36:04 +0200

    ex 1 y ex2

---
+++ index.js
@@ -0,0 +1,17 @@
+// ex1
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+const add = (a, b) => a + b;
+
+console.log (add(3,4))
+
+
+// ex2
+let randomNumber = () => Math.floor(Math.random()* 101) //por 101 para incluir el 100, y redondeando hacia abajo.
+
+console.log (randomNumber())      
+
+
